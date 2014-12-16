1726db8f7189e0ce1ee08cd08c570ac3378c739a
hiuller <hiuller@gmail.com>
2014-12-16 17:43:25 -0200

    working on reblow

--- 08_reblow/analysis.r
+++ 08_reblow/analysis.r
@@ -8,3 +8,15 @@ rawData$mes <- format(as.Date(rawData$DT_PRODC_OBTD_ACI, format="%d/%m/%Y"), for
 rawData$ressopro <- (rawData$VOL_RESS > 40)
 
 tapply(rawData$ressopro, rawData$mes, sum)/table(rawData$mes)
+
+j <- table(rawData$TP_CORR)
+incluir <- names(j)[j>100]
+
+sub <- subset(rawData, subset=rawData$TP_CORR %in% incluir)
+table(as.character(sub$TP_CORR))
+
+sub$TP_CORR <- droplevels(sub$TP_CORR)
+sub$TP_CORR
+
+
+plot(sub$PERC_SUCATA ~ jitter(as.numeric(sub$TP_CORR)), pch=19, col='red', cex=1.2, axis=NA)
